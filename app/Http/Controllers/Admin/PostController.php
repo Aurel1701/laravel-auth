7022280ab276142ed3befd7587da34884165b2e9
Aurel1701 <gantzfan96@gmail.com>
2022-06-23 14:54:27 +0200

    edit PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -81,10 +81,11 @@ class PostController extends Controller
      * @param  \App\Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Post $post)
+    public function update(Post $post)
     {
         //dd($request->all());
         //$val_post = $request->validated();
+        //dd($val_post);
         //$post->update($val_post);
         // redirect to get route
         //return redirect()->route('admin.posts.index');
