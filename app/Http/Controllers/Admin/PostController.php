69ffca7df22d6512351b9547f079e1ef8294276f
Aurel1701 <gantzfan96@gmail.com>
2022-06-23 08:36:04 +0200

    add store in PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -29,7 +29,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -40,7 +40,16 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //dd($request->all());
+        $data = $request ->all();
+        $newPost = new Post();
+        $newPost->title = $data['title'];
+        $newPost->excerpt = $data['excerpt'];
+        $newPost->cover = $data['cover'];
+        $newPost->body = $data['body'];
+        $newPost->author = $data['author'];
+        $newPost->save();
+        return redirect()->route('admin.posts.index');
     }
 
     /**
